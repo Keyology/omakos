e205ca412fad28659bbc97307ecaf57e7a221e8c
keyology <keonimurray45@gmail.com>
2025-10-18 02:04:53 -0400

    removed script clearing doc

--- scripts/mac.sh
+++ scripts/mac.sh
@@ -88,7 +88,6 @@ defaults write com.apple.dock expose-animation-duration -float 0.1
 defaults write com.apple.dock springboard-show-duration -int 0
 defaults write com.apple.dock springboard-hide-duration -int 0
 defaults write com.apple.dock springboard-page-duration -int 0
-defaults write com.apple.dock persistent-apps -array
 defaults write com.apple.dock mru-spaces -bool false
 print_success_muted "Dock preferences configured"
 
